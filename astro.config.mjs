7ba9762ed4856b2769a10248c8db42deadb95625
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2022-11-27 00:47:14 +0100

    feat: add dist output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-// import image from '@astrojs/image';
+import image from '@astrojs/image';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // integrations: [image(), sitemap()],
-  integrations: [sitemap()],
+  integrations: [image(), sitemap()],
+  // integrations: [sitemap()],
   trailingSlash: 'never',
   site: 'https://astucescasinoenligne.com',
 });
