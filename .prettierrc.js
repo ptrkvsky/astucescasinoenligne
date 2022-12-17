8b314de73b0ca3a328240aed2bf325a1fb14fc82
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2022-12-17 18:13:36 +0100

    feat: :sparkles: add pwa

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require("prettier-plugin-astro")
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: { parser: "astro" }
+    }
+  ],
+  trailingComma: "none",
+  semi: false,
+  singleQuote: false,
+}
--- src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  url: import.meta.env.URL,
-  siteName: 'Astuces Casino en Ligne',
-};
-
-export default config;
